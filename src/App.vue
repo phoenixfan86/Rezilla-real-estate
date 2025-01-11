d81565c56d643a9a0e67b4b24fb21ae86782b806
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-01-11 15:48:16 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,7 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script></script>
 
 <template>
-	<header>
-		<img
-			alt="Vue logo"
-			class="logo"
-			src="./assets/logo.svg"
-			width="125"
-			height="125"
-		/>
-
-		<div class="wrapper">
-			<HelloWorld msg="You did it!" />
-		</div>
-	</header>
-
-	<main>
-		<h1>test04</h1>
-		<TheWelcome />
-	</main>
+	<h1>TEST 01</h1>
 </template>
 
-<style scoped>
-header {
-	line-height: 1.5;
-}
-
-.logo {
-	display: block;
-	margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-	header {
-		display: flex;
-		place-items: center;
-		padding-right: calc(var(--section-gap) / 2);
-	}
-
-	.logo {
-		margin: 0 2rem 0 0;
-	}
-
-	header .wrapper {
-		display: flex;
-		place-items: flex-start;
-		flex-wrap: wrap;
-	}
-}
-</style>
+<style scoped></style>
