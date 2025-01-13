806e71e9917485e83cc44095078071c0ff86af90
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-01-13 14:07:36 +0200

    change

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script>
-import Header from "/components/Header.vue";
-import Topnav from "/components/Topnav.vue";
+import Header from "./components/Header.vue";
+import Topnav from "./components/Topnav.vue";
 
 export default {
 	components: { Header, Topnav },
