2744a8f7bb733060f88f91166b1c354776f483e7
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-01-14 15:14:07 +0200

    add button large

--- src/components/Buttons/Buttonlarge.vue
+++ src/components/Buttons/Buttonlarge.vue
@@ -11,9 +11,25 @@ export default {
 </script>
 
 <template>
-  <div class="button">
-    {{ label }}
-  </div>
+  <a href="">
+    <div class="button__large">
+      <slot name="icon"></slot>
+      {{ label }}
+    </div>
+  </a>
 </template>
 
-<style></style>
+<style>
+.button__large {
+  width: 170px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-self: start;
+  gap: 10px;
+  padding: 15px 20px;
+  border-radius: 50px;
+  background-color: var(--primary-colour);
+  color: #fff;
+}
+</style>
--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -5,25 +5,29 @@ import Sitelogo from "./Sitelogo.vue";
 import Userlogin from "./Userlogin.vue";
 
 export default {
-  components: { Button, Sitelogo, Userlogin }
+  components: { Button, Sitelogo, Userlogin, Buttonlarge }
 }
 </script>
 <template>
   <nav>
     <div class="top__menu">
       <ul class="top__menu__wrapper">
-        <li><a href="#" class="menu__item"><Button class="button _text_2" label="Home" /></a></li>
-        <li><a href="#" class="menu__item"><Button class="button _text_2" label="Aboutus" /></a></li>
-        <li><a href="#" class="menu__item"><Button class="button _text_2" label="Listings" /></a></li>
-        <li><a href="#" class="menu__item"><Button class="button _text_2" label="Services" /></a></li>
-        <li><a href="#" class="menu__item"><Button class="button _text_2" label="Blog" /></a></li>
+        <li><a href="#" class="menu__item"><Button class="_text_2" label="Home" /></a></li>
+        <li><a href="#" class="menu__item"><Button class="_text_2" label="Aboutus" /></a></li>
+        <li><a href="#" class="menu__item"><Button class="_text_2" label="Listings" /></a></li>
+        <li><a href="#" class="menu__item"><Button class="_text_2" label="Services" /></a></li>
+        <li><a href="#" class="menu__item"><Button class="_text_2" label="Blog" /></a></li>
       </ul>
     </div>
     <div class="logo">
       <Sitelogo />
     </div>
     <Userlogin />
-    <>
+    <Buttonlarge label="Add Listing">
+      <template #icon>
+        <img src="/src/assets/img/ico/house_ico.png" alt="icon" />
+      </template>
+    </Buttonlarge>
   </nav>
 </template>
 
