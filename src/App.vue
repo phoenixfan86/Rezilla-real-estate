7eb2fb79e189395ccda8c0a308e8ff1df5a10810
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-01-20 15:50:27 +0200

    add content

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@ import Header from "./components/Header.vue";
 import Topnav from "./components/Topnav.vue";
 import RealEstate from "./components/RealEstate.vue";
 import Brands from "./components/Brands.vue";
+import Whoarewe from "./components/Whoarewe.vue";
+
 
 export default {
-	components: { Header, Topnav, RealEstate, Brands },
+	components: { Header, Topnav, RealEstate, Brands, Whoarewe },
 }
 </script>
 
@@ -15,6 +17,7 @@ export default {
 		<Topnav />
 		<RealEstate />
 		<Brands />
+		<Whoarewe />
 	</div>
 </template>
 
--- src/components/Whoarewe.vue
+++ src/components/Whoarewe.vue
@@ -1,4 +1,8 @@
-<script></script>
+<script>
+export default {
+
+}
+</script>
 <template>
   <div class="block__wrapper">
     <div class="inform">
@@ -7,12 +11,23 @@
       <p>Donec porttitor euismod dignissim. Nullam a lacinia ipsum, nec dignissim purus. Nulla convallis ipsum molestie
         nibh malesuada, ac malesuada leo volutpat.</p>
       <div class="sub__block">
-        <img src="" alt="">
+        <img src="/public/img/ico/Vector-1.png" alt="">
         <div class="desc">
           <div class="title">Donec porttitor euismod</div>
           <p>Nullam a lacinia ipsum, nec dignissim purus. Nulla</p>
         </div>
       </div>
+      <div class="sub__block">
+        <img src="/public/img/ico/Vector.png" alt="">
+        <div class="desc">
+          <div class="title">Donec porttitor euismod</div>
+          <p>Nullam a lacinia ipsum, nec dignissim purus. Nulla</p>
+        </div>
+      </div>
+    </div>
+    <div class="img_wrapper">
+      <img src="/public/img/gallery/3.png" alt="">
+      <div class="img__item"><img src="" alt=""><img src="" alt=""></div>
     </div>
   </div>
 </template>
