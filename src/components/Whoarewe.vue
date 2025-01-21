ae2c8d8db850cab483e3e381c15d02528adafbbc
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-01-21 12:28:21 +0200

    add style

--- src/components/Whoarewe.vue
+++ src/components/Whoarewe.vue
@@ -8,26 +8,29 @@ export default {
     <div class="inform">
       <div class="_label">who are we</div>
       <h2>Assisting individuals in locating the appropriate real estate.</h2>
-      <p>Donec porttitor euismod dignissim. Nullam a lacinia ipsum, nec dignissim purus. Nulla convallis ipsum molestie
+      <p class="_text_1">Donec porttitor euismod dignissim. Nullam a lacinia ipsum, nec dignissim purus. Nulla convallis
+        ipsum molestie
         nibh malesuada, ac malesuada leo volutpat.</p>
       <div class="sub__block">
         <img src="/public/img/ico/Vector-1.png" alt="">
         <div class="desc">
-          <div class="title">Donec porttitor euismod</div>
-          <p>Nullam a lacinia ipsum, nec dignissim purus. Nulla</p>
+          <div class="_label">Donec porttitor euismod</div>
+          <p class="_text_1">Nullam a lacinia ipsum, nec dignissim purus. Nulla</p>
         </div>
       </div>
       <div class="sub__block">
         <img src="/public/img/ico/Vector.png" alt="">
         <div class="desc">
-          <div class="title">Donec porttitor euismod</div>
-          <p>Nullam a lacinia ipsum, nec dignissim purus. Nulla</p>
+          <div class="_label">Donec porttitor euismod</div>
+          <p class="_text_1">Nullam a lacinia ipsum, nec dignissim purus. Nulla</p>
         </div>
       </div>
     </div>
     <div class="img_wrapper">
-      <img src="/img/gallery/3.png" alt="">
       <div class="img__item">
+        <img src="/img/gallery/3.png" alt="">
+      </div>
+      <div class="img__item_1">
         <img src="/public/img/gallery/2.png" alt="">
         <img src="/public/img/gallery/4.png" alt="">
       </div>
@@ -40,13 +43,58 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
+  gap: 30px;
+  margin-top: 180px;
 }
 
-.img_wrapper {
-  width: 100%;
+.inform {
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  gap: 30px;
+}
+
+.sub__block {
+  width: 400px;
   display: flex;
   align-items: center;
   justify-content: center;
   gap: 20px;
+  padding: 20px;
+}
+
+.sub__block img {
+  width: 40px;
+  height: 40px;
+}
+
+.img_wrapper {
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+}
+
+.img__item {
+  width: 280px;
+  height: 500px;
+}
+
+.img__item img {
+  width: 280px;
+  height: 500px;
+}
+
+.img__item_1 {
+  width: 280px;
+  height: 550px;
+}
+
+.img__item_1:nth-child(1) {
+  width: 277px;
+  height: 280px;
 }
 </style>
